029ee8fbf5d77cebd9a4f13bfc30e2ac7563b36a
Alex Hussey <ahussey@redhat.com>
2024-09-12 17:40:55 +1000

    fix: OIDC token refresh

--- src/cli/adapters/KeycloakInterface.py
+++ src/cli/adapters/KeycloakInterface.py
@@ -1,15 +1,16 @@
 #
 # Used to provide a common interface for Keycloak integration
 #
+import cli.configuration.variables as app_variables
+
 import os
 import json
+import logging
 import datetime
 import base64
 
 from typer import prompt, Argument
 from keycloak import KeycloakOpenID
-
-HOME_DIR = os.environ["HOME"]
 
 def generate_token(keycloak_openid):
     username = prompt("What's your username?")
@@ -20,9 +21,9 @@ def generate_token(keycloak_openid):
         grant_type="password",
         scope="openid"
     )
-    with open(f"{HOME_DIR}/.config/.silent-auction.txt", "w") as fp:
+    with open(f"{app_variables.HOME_DIR}/.config/.silent-auction.txt", "w") as fp:
         fp.write(json.dumps(token))
-    return token
+    app_variables.TOKEN = token
 
 class KeycloakInterface:
     def __init__(self):
@@ -38,30 +39,27 @@ class KeycloakInterface:
 
     def get_token(self):
         try:
-            with open(f"{HOME_DIR}/.config/.silent-auction.txt", "r") as fp:
-                load_token = json.loads(fp.read().strip())
-                access_token_parts = load_token['access_token'].split('.')
-                access_token_payload = json.loads(base64.urlsafe_b64decode(access_token_parts[1] + '=' * (4 - len(access_token_parts[1]) % 4)))
+            app_variables.TOKEN = app_variables.LOAD_TOKEN_FROM_DISK
+            access_token_parts = app_variables.TOKEN['access_token'].split('.')
+            access_token_payload = json.loads(base64.urlsafe_b64decode(access_token_parts[1] + '=' * (4 - len(access_token_parts[1]) % 4)))
 
-                now = datetime.datetime.now()
-                access_token_exp = datetime.datetime.fromtimestamp(access_token_payload['exp'])
-                access_token_exp_minus_seconds = access_token_exp - datetime.timedelta(seconds=100)
+            now = datetime.datetime.now()
+            access_token_exp = datetime.datetime.fromtimestamp(access_token_payload['exp'])
+            access_token_exp_minus_seconds = access_token_exp - datetime.timedelta(seconds=100)
 
-                if now > access_token_exp:
-                    print("Session expired. Please log in again.")
-                    return generate_token(self.keycloak_openid)
-                if access_token_exp_minus_seconds <= now < access_token_exp:
-                    # print("Token expired. Refreshing")
-                    try:
-                        self.token = self.refresh_token(load_token)
-                        return self.token
-                    except BaseException as e:
-                        print(e)
-                        return generate_token(self.keycloak_openid)
-                return load_token
+            if now > access_token_exp:
+                logging.warning("Session expired. Please log in again.")
+                return generate_token(self.keycloak_openid)
+            if access_token_exp_minus_seconds <= now < access_token_exp:
+                logging.debug("Token expired. Refreshing")
+                try:
+                    self.refresh_token(app_variables.TOKEN)
+                except BaseException as e:
+                    print(e)
+                    generate_token(self.keycloak_openid)
         except BaseException as e:
-            print(e)
-            return generate_token(self.keycloak_openid)
+            logging.warning(e)
+            generate_token(self.keycloak_openid)
 
     def get_user_info(self):
         # Get Userinfo
@@ -70,11 +68,13 @@ class KeycloakInterface:
 
     def refresh_token(self, token):
         # Refresh token
-        with open(f"{HOME_DIR}/.config/.silent-auction.txt", "w") as fp:
+        with open(f"{app_variables.HOME_DIR}/.config/.silent-auction.txt", "w") as fp:
             token = self.keycloak_openid.refresh_token(token['refresh_token'])
             fp.write(json.dumps(token))
         return token
 
-    def logout(self):
+    def logout(self, token):
         # Logout
-        self.keycloak_openid.logout(self.token['refresh_token'])
+        self.keycloak_openid.logout(token['refresh_token'])
+        with open(f"{app_variables.HOME_DIR}/.config/.silent-auction.txt", "w") as fp:
+            fp.write(json.dumps("{}"))
--- src/cli/configuration/variables.py
+++ src/cli/configuration/variables.py
@@ -1,11 +1,17 @@
 from cli.adapters.KeycloakInterface import KeycloakInterface
 
 import os
+import json
 import logging
 
+# Global vars
+HOME_DIR = os.environ["HOME"]
+
 # Auth
+TOKEN = {}
 KC = KeycloakInterface()
-TOKEN = KC.get_token()
+with open(f"{HOME_DIR}/.config/.silent-auction.txt", "r") as fp:
+    LOAD_TOKEN_FROM_DISK = json.loads(fp.read().strip())
 
 # Backend
 BACKEND_BASE_URL=os.environ.get("BACKEND_BASE_URL")
